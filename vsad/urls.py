c9a89806abe8737491a048fe39df49351f432921
saihminosil <informatica3.saih@chminosil.es>
2025-03-27 14:08:20 +0100

    vsad app limpiar archivos

--- vsad/urls.py
+++ vsad/urls.py
@@ -4,8 +4,6 @@ from . import views
 from . import requests
 
 urlpatterns = [    
-    path('/<str:codigo_estacion_txt>',views.estacion),
     path('/datos/<str:codigo_estacion_txt>',requests.grafica_estacion),
     path('',views.estaciones),
-    path('/antigua/<str:codigo_estacion_txt>',views.estacion_antigua),
 ]
--- vsad/views.py
+++ vsad/views.py
@@ -2,13 +2,6 @@ from django.shortcuts import render
 from .models import FewsSeries,InfoEstacion,InfoVariable,VariableForecast
 from django.db.models import Max
 
-def estacion(request,codigo_estacion_txt):
-    estacion=InfoEstacion.objects.get(estacionid=codigo_estacion_txt)
-    historica=VariableForecast.objects.filter(estacion_id=codigo_estacion_txt).first()
-    forecast=historica.variableforecast
-    fecha_ultima_prediccion=FewsSeries.objects.filter(seriesid=forecast).aggregate(Max('fechaforecast'))['fechaforecast__max']
-    return render(request, 'prediccion.html', {"estacion": estacion,"historica":historica,"fecha_prediccion":fecha_ultima_prediccion}) 
-
 def estaciones(request):
     estaciones=VariableForecast.objects.all().order_by('estacion')#.values('le_codigo_txt','le_nombre','le_rio','le_tipologia','le_tipo_sensor')
     #for estacion in estaciones:
@@ -22,10 +15,3 @@ def estaciones(request):
     print(estaciones)
     return render(request,'estaciones_saih.html',{"estaciones":estaciones})    
 
-def estacion_antigua(request,codigo_estacion_txt):
-    print("estacion antigua")
-    estacion=InfoEstacion.objects.get(estacionid=codigo_estacion_txt)
-    historica=VariableForecast.objects.filter(estacion_id=codigo_estacion_txt).first()
-    forecast=historica.variableforecast
-    fecha_ultima_prediccion=FewsSeries.objects.filter(seriesid=forecast).aggregate(Max('fechaforecast'))['fechaforecast__max']
-    return render(request, 'prediccion.html', {"estacion": estacion,"historica":historica,"fecha_prediccion":fecha_ultima_prediccion}) 
